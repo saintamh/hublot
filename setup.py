b741e9e2f2714ac61332016e4072859d80ce7a84
Hervé Saint-Amand <git@saintamh.org>
2021-03-13 07:46:19 +0000

    Declare the package as having type annotations

--- setup.py
+++ setup.py
@@ -23,6 +23,7 @@ setuptools.setup(
     version=FORBAN_VERSION,
     author='Hervé Saint-Amand',
     packages=setuptools.find_packages(),
+    package_data={'forban': ['py.typed']},
     install_requires=[
         'dataclasses>=0.8,<1; python_version<"3.7"',
         'requests>=2.25,<3',
@@ -32,4 +33,5 @@ setuptools.setup(
         'Operating System :: OS Independent',
         'Programming Language :: Python :: 3',
     ],
+    zip_safe=False, # https://mypy.readthedocs.io/en/latest/installed_packages.html#creating-pep-561-compatible-packages
 )
